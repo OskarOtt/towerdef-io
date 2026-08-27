ff64d20adc39c32cd34d715b267270edd59a087d
OskarOtt <oskar.ott@icloud.com>
2026-08-27 19:45:16 +0200

    remove cringe free to play comment

--- src/components/WelcomeModal.tsx
+++ src/components/WelcomeModal.tsx
@@ -32,8 +32,6 @@ ___  __        ___  __   __   ___  ___     __
           &gt; DEPLOY TOWERS ALONG THE PATH. DEFEND THE CORE.
           <br />
           &gt; EARN GOLD. SURVIVE THE WAVES.
-          <br />
-          &gt; FREE TO PLAY. NO DOWNLOAD REQUIRED.
         </p>
         <div className="modal-actions">
           <button className="term-btn" onClick={handleNewGameClick}>
